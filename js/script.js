e136e447c282b949e07ad3f59c858c3f7d4e7f55
Neivanh <sonix88@list.ru>
2022-11-15 15:39:03 +0600

    burger_menu

--- js/script.js
+++ js/script.js
@@ -34,5 +34,18 @@ $( function() {
   });
 } );
 
+//bruger
+const burgerBtn = document.querySelector('.burger');
+const menuClose = document.querySelector('.menu-close');
+const menuBurger = document.querySelector('.nav');
+
+burgerBtn.addEventListener('click', () => {
+  menuBurger.classList.add('burger-active');
+});
+
+menuClose.addEventListener('click', () => {
+  menuBurger.classList.remove('burger-active');
+});
+
 
 
